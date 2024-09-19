e6e4e77b82ad50a8bd803fde78e7694da3e12887
Jonathannygaard <jonathan.nygaard1310@gmail.com>
2024-09-19 10:02:37 +0200

    Fuck c#

--- SMA_3_Csharp/Program.cs
+++ SMA_3_Csharp/Program.cs
@@ -1,9 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-public class Program
+using System.Numerics;
+
+
+
+public class Enemy
 {
-    public static void Main(string[] args)
+    public Vector3 m_position;
+    public Vector3 m_direction;
+    public float m_velosity;
+
+    public void Move()
     {
-        Console.WriteLine("Hello, World!");
+        m_position += m_direction * m_velosity;
     }
 }
+
+public class EnemyData()
+{
+    public int NumEnemies;
+    public Vector3[] Positions;
+    public Vector3[] Directions;
+    public float[] Velocities;
+
+    public void MoveAllEnemies()
+    {
+        for(int i = 0; i < NumEnemies; i++)
+        {
+            Positions[i] += Directions[i] * Velocities[i];
+        }
+    }
+}
+public class Programs
+ {
+     public static void Main(string[] args)
+     {
+         Console.WriteLine("Hello, World!");
+     }
+ }
